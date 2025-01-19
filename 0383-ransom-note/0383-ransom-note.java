e8ed26b7c3d27ffe0cd63a41fb31475ea9764868
Dongmin Kim <ddmkim94@gmail.com>
2025-01-19 22:37:28 +0900

    Time: 10 ms (60.06%), Space: 45.8 MB (6.17%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < magazine.length(); i++) {
+            map.put(magazine.charAt(i), map.getOrDefault(magazine.charAt(i), 0) + 1);
+        }
+
+        for (int i = 0; i < ransomNote.length(); i++) {
+
+            char ch = ransomNote.charAt(i);
+
+            if (map.containsKey(ch) && map.get(ch) > 0) {
+                map.put(ch, map.get(ch) - 1);
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}

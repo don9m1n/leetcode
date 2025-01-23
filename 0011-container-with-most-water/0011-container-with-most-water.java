3ee8935e5af2ee809fca3f423619de609204826d
Dongmin Kim <ddmkim94@gmail.com>
2025-01-23 18:00:00 +0900

    Time: 5 ms (74%), Space: 57.2 MB (91.31%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1;
+        int max = Integer.MIN_VALUE;
+
+        while (left < right) {
+
+            max = Math.max(max, Math.min(height[left], height[right]) * (right - left));
+
+            if (height[right] > height[left]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return max;
+    }
+}

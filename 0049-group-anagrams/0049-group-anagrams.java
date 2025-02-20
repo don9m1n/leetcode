943e7d233367ca307ac1cd6622d3602cd44da8e1
Dongmin Kim <dmk94@naver.com>
2025-02-20 19:18:01 +0900

    Time: 7 ms (65.99%), Space: 47.8 MB (68.57%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String word : strs) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+
+            String sortedWord = String.valueOf(chars);
+
+            if(!map.containsKey(sortedWord)) {
+                map.put(sortedWord, new ArrayList<>());
+            }
+
+            map.get(sortedWord).add(word);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}

437037447acae97d59a70762d8c955c999c33824
Dongmin Kim <ddmk1228@gmail.com>
2025-07-09 18:13:11 +0900

    Time: 1 ms (99.43%), Space: 43.9 MB (88.91%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,24 +1,28 @@
 class Solution {
     public int compress(char[] chars) {
         int s = 0;
-        int e = 1;
+        int e = s + 1;
 
         StringBuilder sb = new StringBuilder();
         while(e < chars.length) {
             if (chars[s] != chars[e]) {
-
-                int length = e - s;
-                // 그룹 문자 생성
                 sb.append(chars[s]);
-                sb.append(length == 1 ? "" : length);
+
+                if (e - s > 1) {
+                    sb.append(e - s);
+                }
+
                 s = e;
             }
 
             e++;
         }
-        int length = e - s;
+
         sb.append(chars[s]);
-        sb.append(length == 1 ? "" : length);
+        
+        if (e - s > 1) {
+            sb.append(e - s);
+        }
 
         for (int i = 0; i < sb.length(); i++) {
             chars[i] = sb.charAt(i);

c15986403b3299bdd71f89e0f696fdfd15909da6
Dongmin Kim <ddmk1228@gmail.com>
2025-07-16 18:49:16 +0900

    Time: 73 ms (22.27%), Space: 45.8 MB (37.57%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -1,23 +1,14 @@
 class Solution {
     public boolean closeStrings(String word1, String word2) {
-        Set<Character> set1 = new HashSet<>();
-        Set<Character> set2 = new HashSet<>();
-
         Map<Character, Integer> map1 = new HashMap<>();
         Map<Character, Integer> map2 = new HashMap<>();
 
         for (int i = 0; i < word1.length(); i++) {
-            set1.add(word1.charAt(i));
             map1.put(word1.charAt(i), map1.getOrDefault(word1.charAt(i), 0) + 1);
         }
 
         for (int i = 0; i < word2.length(); i++) {
-            set2.add(word2.charAt(i));
             map2.put(word2.charAt(i), map2.getOrDefault(word2.charAt(i), 0) + 1);
-        }
-
-        if (!set1.equals(set2)) {
-            return false;
         }
 
         List<Integer> list1 = new ArrayList<>(map1.values());
@@ -25,12 +16,6 @@ class Solution {
         Collections.sort(list1);
         Collections.sort(list2);
 
-        for (int i = 0; i < list1.size(); i++) {
-            if(!Objects.equals(list1.get(i), list2.get(i))){
-                return false;
-            }
-        }
-
-        return true;
+        return map1.keySet().equals(map2.keySet()) && list1.equals(list2);
     }
 }

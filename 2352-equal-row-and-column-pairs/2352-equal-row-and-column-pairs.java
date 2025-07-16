28cfb032cb83114333d555dd0f3ce6396faa50d0
Dongmin Kim <ddmk1228@gmail.com>
2025-07-16 19:38:32 +0900

    Time: 23 ms (76.82%), Space: 52 MB (34.99%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
@@ -1,28 +1,28 @@
 class Solution {
     public int equalPairs(int[][] grid) {
-        Map<String, Integer> map1 = new HashMap<>();
-        Map<String, Integer> map2 = new HashMap<>();
+        Map<List<Integer>, Integer> rowMap = new HashMap<>();
+        Map<List<Integer>, Integer> colMap = new HashMap<>();
 
         for (int i = 0; i < grid.length; i++) {
-            StringBuilder num1 = new StringBuilder();
-            StringBuilder num2 = new StringBuilder();
+            List<Integer> row = new ArrayList<>();
+            List<Integer> col = new ArrayList<>();
 
             for (int j = 0; j < grid.length; j++) {
-                num1.append(grid[i][j]).append(" ");
-                num2.append(grid[j][i]).append(" ");
+                row.add(grid[i][j]);
+                col.add(grid[j][i]);
             }
 
-            map1.put(num1.toString(), map1.getOrDefault(num1.toString(), 0) + 1);
-            map2.put(num2.toString(), map2.getOrDefault(num2.toString(), 0) + 1);
+            rowMap.put(row, rowMap.getOrDefault(row, 0) + 1);
+            colMap.put(col, colMap.getOrDefault(col, 0) + 1);
         }
 
-        int count = 0;
-        for (String key : map1.keySet()) {
-            if (map2.containsKey(key)) {
-                count += map1.get(key) * map2.get(key);
+        int pair = 0;
+        for (List<Integer> key : rowMap.keySet()) {
+            if (colMap.containsKey(key)) {
+                pair += rowMap.get(key) * colMap.get(key);
             }
         }
 
-        return count;
+        return pair;
     }
 }
